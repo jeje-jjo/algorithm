f3ac2fba68c5fe494b3f5e3ca7f61ce3dc3254a8
제제 <heyj.jjo@gmail.com>
2024-04-02 17:05:18 +0900

    10820 풀이완료

--- data_structure_part_01/Q10820.java
+++ data_structure_part_01/Q10820.java
@@ -7,17 +7,32 @@ public class Q10820 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[][] arr = new int[100][];
-        String str = "";
+        String s;
 
-        long end = System.currentTimeMillis() + 1000;
+        while((s = br.readLine()) != null){
 
-        while(System.currentTimeMillis() < end){
-            if(System.in.available() > 0){
-               str = br.readLine();
-               end = System.currentTimeMillis() + 1000;
+            int[] arr = new int[4];
+
+            for(int i = 0; i < s.length() ; i++){
+
+                if(s.charAt(i) >= 'a' && s.charAt(i) <= 'z'){
+                    arr[0] += 1;
+                }else if(s.charAt(i) >= 'A' && s.charAt(i) <= 'Z'){
+                    arr[1] += 1;
+                }else if(s.charAt(i) >= '0' && s.charAt(i) <= '9'){
+                    arr[2] += 1;
+                }
+
+                if(s.charAt(i) == ' '){
+                    arr[3] += 1;
+                }
             }
 
+            for(int i : arr){
+                bw.write(i + " ");
+            }
+
+            bw.write("\n");
         }
 
 
