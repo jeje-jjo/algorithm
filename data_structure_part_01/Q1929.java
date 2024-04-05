0e511c4c776264ed8e2e9d60cce6d865f0d4f10a
제제 <heyj.jjo@gmail.com>
2024-04-05 12:08:01 +0900

    시간 단축하기
    아주 성공적!

--- data_structure_part_01/Q1929.java
+++ data_structure_part_01/Q1929.java
@@ -18,6 +18,9 @@ public class Q1929 {
         int fst = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
 
+        list.add(2);
+        list.add(3);
+
         for(int i = 2; i <= end; i++){
 
             if(prime(i)){
@@ -39,19 +42,23 @@ public class Q1929 {
 
         if(num <= 1) return false;
 
-        for (int i : list) {
-            if (i * i <= num) {
-                if (num % i == 0) {
-                    return false;
-                }
-            }
-        }
-
-//        for(int i = 2 ; i * i <= num; i++){
-//            if(num % i == 0){
-//                return false;
+//        for (int i : list) {
+//
+//            if (i * i <= num) {
+//                if (num % i == 0) {
+//                    return false;
+//                }
 //            }
 //        }
+
+        for(int i = 0; list.get(i)* list.get(i) <= num; i++){
+
+
+                if(num % list.get(i) == 0){
+                    return false;
+                }
+
+        }
 
         return true;
     }
