21822e23d4d1085826a05da462a5598cc4f60a39
제제 <heyj.jjo@gmail.com>
2024-04-05 11:43:28 +0900

    1929 풀이완료

---
+++ data_structure_part_01/Q1929.java
@@ -0,0 +1,58 @@
+package data_structure_part_01;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Q1929 {
+
+    static List<Integer> list = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int fst = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        for(int i = 2; i <= end; i++){
+
+            if(prime(i)){
+                list.add(i);
+
+                if(i >= fst) bw.write(i + "\n");
+            }
+        }
+
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
+    public static boolean prime(int num){
+
+        if(num <= 1) return false;
+
+        for (int i : list) {
+            if (i * i <= num) {
+                if (num % i == 0) {
+                    return false;
+                }
+            }
+        }
+
+//        for(int i = 2 ; i * i <= num; i++){
+//            if(num % i == 0){
+//                return false;
+//            }
+//        }
+
+        return true;
+    }
+}
