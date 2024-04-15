4398f8111a3da3927b46ef67bae6f5332c6f43f4
제제 <heyj.jjo@gmail.com>
2024-04-15 18:34:24 +0900

    10844 풀이

---
+++ data_structure_part_01/Q10844.java
@@ -0,0 +1,16 @@
+package data_structure_part_01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q10844 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] bp = new int[]
+    }
+}
