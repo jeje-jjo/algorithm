cc86d5c73069f2a47ae43f310727cddc37646db2
제제 <heyj.jjo@gmail.com>
2024-04-18 16:29:14 +0900

    10844 풀이완료

--- data_structure_part_01/Q10844.java
+++ data_structure_part_01/Q10844.java
@@ -6,10 +6,54 @@ import java.io.InputStreamReader;
 
 public class Q10844 {
 
+    static Long[][] dp;
+    static long mod = 1000000000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
+        dp = new Long[n+1][10];
 
+        for(int i = 0; i < 10; i++){
+            dp[1][i] = 1L;
+        }
+
+        long sum = 0;
+        for(int i = 1; i <= 9; i++){
+            sum += cal(n, i);
+        }
+
+        System.out.println(sum % mod);
+
+        br.close();
+
+
+    }
+
+
+    static long cal(int num, int val){
+
+
+        if(num == 1){
+            return dp[num][val];
+
+        }
+
+        if(dp[num][val] == null ) {
+
+
+            if (val == 0) {
+                dp[num][val] = cal(num - 1, 1);
+            } else if (val == 9) {
+                dp[num][val] = cal(num - 1, 8);
+            } else {
+                dp[num][val] = cal(num - 1, val - 1) + cal(num - 1, val + 1);
+            }
+
+        }
+
+
+
+        return dp[num][val] % mod;
     }
 }
