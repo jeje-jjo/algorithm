be7b1e696ecf24e2778e73a4e1bff4575ba534bc
제제 <heyj.jjo@gmail.com>
2024-04-23 17:05:46 +0900

    2193 풀이중

---
+++ data_structure_part_01/Q2193.java
@@ -0,0 +1,12 @@
+package data_structure_part_01;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Q2193 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        
+    }
+}
