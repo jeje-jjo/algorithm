32d0edcc714299c7d1075ea3222947e6d6946cd6
제제 <heyj.jjo@gmail.com>
2024-04-26 16:25:33 +0900

    1913 풀이완료

--- data_structure_part_01/Q1912.java
+++ data_structure_part_01/Q1912.java
@@ -12,10 +12,28 @@ public class Q1912 {
         int n = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
 
-        for(int i = 0; i < n ; i++){
+
+        int[] nums = new int[n+1];
+        int[] dp = new int[n+1];
+
+        for(int i = 1; i <= n ; i++){
+
+            nums[i] = Integer.parseInt(st.nextToken());
 
         }
+
+        int max = nums[1];
+
+        for(int i = 1; i <= n ; i++){
+
+            dp[i] = Math.max(dp[i-1] + nums[i], nums[i]);
+
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+
+        br.close();
     }
 }
