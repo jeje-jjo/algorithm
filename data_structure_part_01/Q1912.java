5472210c5150d2819c3b667f36754bc1c6914596
제제 <heyj.jjo@gmail.com>
2024-04-26 14:45:03 +0900

    1912  풀이중

---
+++ data_structure_part_01/Q1912.java
@@ -0,0 +1,21 @@
+package data_structure_part_01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q1912 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+
+        for(int i = 0; i < n ; i++){
+
+        }
+    }
+}
